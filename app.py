a474356bd5c18ed8eebf93ac2ea4fd9aca46f7b7
glaserdi <glaserdiuvegezo@gmail.com>
2025-02-27 18:31:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def main_content():
                       df.iterrows()]
 
             # Naptár megjelenítése
-            calendar_options = {"initialView": "dayGridMonth", "firstDay": 1, "locale": "hu"}
+            calendar_options = {"initialView": "dayGridMonth", "firstDay": 1, "locale": "hu", ["dayGridMonth", "listMonth"]}
             calendar(events, options=calendar_options)
 
         except Exception as e:
