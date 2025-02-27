24ea1079fdc95c0aebb69d5c074f82087562eceb
glaserdi <glaserdiuvegezo@gmail.com>
2025-02-27 19:55:36 +0200

    Update generate_price_offer.py

--- generate_price_offer.py
+++ generate_price_offer.py
@@ -23,7 +23,7 @@ def generate_pdf(order_data, company_logo_path, pecset_path , bevitel=None, sors
     pdf.set_font("Arial", "B", 14)
     pdf.cell(0, 10, "ÁRAJÁNLAT", ln=1, align="C")
     pdf.set_font("Arial", "", 12)
-    pdf.cell(0, 10, f"Dátum: {datetime.now().strftime('%Y-%m-%d')}", ln=0.1, align="C")
+    pdf.cell(0, 15, f"Dátum: {datetime.now().strftime('%Y-%m-%d')}", ln=0.1, align="C")
     pdf.cell(0, 4, f"Ajánlatot kérte: {order_data['Megrendelo_neve'].iloc[0]}", ln=0.1, align="C")
     if bevitel == "file":
         pdf.cell(0, 10,f"Sorszám: {order_data['Sorszám_Megrendelés'].iloc[0]}", ln=0.1, align="C")
@@ -121,7 +121,7 @@ def generate_pdf(order_data, company_logo_path, pecset_path , bevitel=None, sors
     pdf.set_font("Arial", "B", 10)
     pdf.cell(0, 10, "Megjegyzések:", ln=1)
 
-    pdf.set_font("Arial", "B", 7)
+    pdf.set_font("Arial", "", 7)
     pdf.cell(0, 4, "A kis terület adalék +20%, ha a terület kisebb mint 0.3 m².", ln=1)
     pdf.cell(0, 4, "Extra méret adalék +20%, ha a terület nagyobb mint 2.5 m².", ln=1)
     pdf.cell(0, 4, "MP = Meleg peremmel", ln=1)
