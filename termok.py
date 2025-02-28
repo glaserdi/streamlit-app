a9def08611a76d081ed58eb4ed2d43bc241888a1
glaserdi <glaserdiuvegezo@gmail.com>
2025-02-28 22:10:49 +0200

    Update termok.py

--- termok.py
+++ termok.py
@@ -384,6 +384,7 @@ def show(user_role: str, user_name:str):
                 
                     st.success(f"✅ A kérésed hozzáadtuk a naptárunkhoz: {task_name} - {hatarido}")
                     del st.session_state.adathalmaz
+                    time.sleep(3) 
                     st.rerun()
     
                 else:
